d7ceb1dc1c1bd630e21c6a9972aa874167a46b70
Kevin Fox <39808977+kfoxIsProgrammer@users.noreply.github.com>
2020-04-18 22:49:41 -0400

    Calculate how many binary numbers differ in twoint

---
+++ HammerDistance.java
@@ -0,0 +1,47 @@
+class HammerDistance {
+    public int hammingDistance(int x, int y) {
+        
+        String z ="";
+        String k ="";
+        
+        //Convert x into binary
+        while(x > 0)
+        {
+            int a = x % 2;
+            z = a + z;
+            x = x / 2;
+        }
+        //Convert y into binary
+        while(y > 0)
+        {
+            int b = y % 2;
+            k = b + k;
+            y = y / 2;
+        }
+        
+        //Padding Zeros to make the binaries equal length
+        while(k.length()!=z.length())
+        {
+            if(k.length() > z.length()){
+                z = "0"+z;
+            }
+            else
+            {
+                k = "0"+k;
+            }
+        }
+    
+        //Counting the differences
+        int counter = 0;
+        for(int i = 0; i<k.length(); i++){
+           if(k.charAt(i) != z.charAt(i)){
+               counter++;
+           } 
+        }
+        
+        
+        return counter;
+       
+        
+    }
+}
